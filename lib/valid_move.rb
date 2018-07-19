8d8c13213a2a1a1fe52be2dbfe255d52d95a7faa
KEVIN D Steele <kevindsteeleii@gmail.com>
2018-07-19 03:51:00 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def vaild_move? (board, index)
-  if (board[index] && position_taken?(board[index]))
+  if (board[index] && position_taken?(board[index]) == false)
     return true
   else
     return nil
